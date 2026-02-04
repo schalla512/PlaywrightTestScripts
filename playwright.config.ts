104669d7b950c53f20ad7446cfdb5a2f0555acbf
AS1219 <vanteru.anoopreddy7@gmail.com>
2026-02-04 18:49:53 +0530

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,6 @@ export default defineConfig({
     ['html'],
     ['list'],
     ['junit',{outputFile: 'junit-test-report.xml'}],
-    ['allure-playwright'],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
